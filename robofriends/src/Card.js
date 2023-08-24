008501b48bd94eb90e078e468c3c4ce68232a74c
bunn1 <nicjohbun@gmail.com>
2023-08-24 10:24:09 +0200

    props

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 
-const Card = () => {
+const Card = (props) => {
+    // destruct - {props.email} o {props.name} etc gör som nedan. Förenklar allt
+    const { name, email, id } = props;
   return (
 
     <div className='w-30'>
         
-  <div className='flex flex-column bg-light-green dib br3 pa4 ma2 grow bw2 shadow-5'>
+  <div className='tc flex flex-column bg-light-green dib br3 pa4 ma2 grow bw2 shadow-5'>
      {/* robohash ett API - efter robohash.org/ skriv vad som helst genererar slumpmässig robot */}
-    <img alt='robots' src="https://robohash.org/test?200x200" />
+    <img alt='robots' src={ `https://robohash.org/${props.id}?200x200` }  />
     <div>
-      <h2>Jane Doe</h2>
-      <p>jane.doe@gmail.com</p>
+      <h2>{name}</h2>
+      <p>{email}</p>
     </div>
   </div>
 </div>
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -4,15 +4,17 @@ import "./index.css";
 import Card from "./Card.js";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
+// Export default behöver inga klamrar runt sig. Endast en variabel som skall visas. Om man exporterar utan default (flera variabler) behövs klamrar.
+import { robots } from "./robots.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* Visar sidan på nätet */}
     <div>
-      <Card />
-      <Card />
-      <Card />
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Card id={robots[1]} name={robots[1].name} email={robots[1].email} />
+      <Card id={robots[2]} name={robots[2].name} email={robots[2].email} />
     </div>
   </React.StrictMode>
 );
