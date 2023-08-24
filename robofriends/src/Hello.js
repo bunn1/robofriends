9408f16aa09d8a198f31d04afdae59cc59364cd2
bunn1 <nicjohbun@gmail.com>
2023-08-24 08:27:12 +0200

    create css file, create Hello component

---
+++ robofriends/src/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Hello.css'
+
+class Hello extends React.Component {
+    render () { 
+        return ( 
+        <div>
+        <h1>Hello World</h1>
+        <h1>Fly Fly</h1>
+        </div>
+        )
+    }
+}
+
+export default Hello;
